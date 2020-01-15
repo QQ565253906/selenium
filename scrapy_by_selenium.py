d0b99016f7545caa0ec090cf5309597ee8a3467e
QQ565253906 <565253906@qq.com>
2020-01-16 00:17:05 +0800

    download excel OK

--- scrapy_by_selenium.py
+++ scrapy_by_selenium.py
@@ -1,16 +1,37 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+import time
+from datetime import datetime
+import pdb
+
 options = Options()
 #options.add_argument('--headless')
 #options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
+#options.add_arguhoment('--disable-dev-shm-usage')
 prefs = {'profile.default_content_settings.popups': 1, 'download.default_directory': 'E:\\Django\\website\\selenium'}
 options.add_experimental_option('prefs', prefs)
-driver = webdriver.Chrome(executable_path="E:\Django\website\selenium\chromedriver.exe",chrome_options=options)
+driver = webdriver.Chrome(executable_path="E:\Django\website\selenium\chromedriver.exe",options=options)
 # driver.get('http://localhost:8000/student/dl')
 driver.get('http://localhost:8000/student/login')
+username = driver.find_element_by_id("id_username")
+username.send_keys('mtk40721')
+password = driver.find_element_by_id("id_password")
+password.send_keys('123')
+submit = driver.find_element_by_xpath("//input[@type='submit']")
+submit.click()
+# pdb.set_trace()
 
-#driver.find_element_by_link_text("download").click()
+#in dl page->download won't effect
+dl=driver.find_element_by_link_text("download")
+dl.click()
+
+#in csv page->download will effect
+dl=driver.find_element_by_link_text("download")
+dl.click()
+print(datetime.now())
+time.sleep(1    )
+print(datetime.now())
+# driver.quit()
 
 # from selenium import webdriver # 从selenium导入webdriver
 # options = webdriver.ChromeOptions()
